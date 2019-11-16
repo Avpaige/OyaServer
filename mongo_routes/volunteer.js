9aa97a0b16b5d21893092fe2340292361af4414f
ingan <isabelngan27@gmail.com>
2019-11-15 22:05:24 -0800

    adding comments

--- mongo_routes/volunteer.js
+++ mongo_routes/volunteer.js
@@ -21,11 +21,12 @@ router
     .route("notification/:mysqlID")
     .get(volunteerController.getMessageVolunteer) // FE: account
 
-// Matches with "/volunteer/avail/chat" (in account)
+// Matches with "/volunteer/avail/chat" (in jobs (in users))
 router
     .route("/avail/chat")
     .put(volunteerController.chatAvailUpdate)
 
+// Matches with "/volunteer/stop/chat" (in v-messages)
 router
     .route("/stop/chat")
     .put(volunteerController.finishChat)
