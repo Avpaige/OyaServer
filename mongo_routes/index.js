4afbd9f685e7e1bfb6ecc5017f3c2aac450c9e7b
Ari Virgil-Paige <avpaige@gmail.com>
2019-11-20 21:11:32 -0800

    cleaning up code

--- mongo_routes/index.js
+++ mongo_routes/index.js
@@ -4,9 +4,9 @@ const volunteerRoute = require("./volunteer.js");
 const userRoutes = require("./user.js");
 const socketRoutes = require("./socket.js");
 
-// Routes
+
 router.use("/volunteer", volunteerRoute);
 router.use("/user", userRoutes);
-// router.use("/socket", socketRoutes);
+
 
 module.exports = router;
