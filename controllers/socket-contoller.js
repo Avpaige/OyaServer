c8fc49f777e043024d3bfe9679db64ec60230bfb
ingan <isabelngan27@gmail.com>
2019-11-21 19:52:25 -0800

    addign rooms

--- controllers/socket-contoller.js
+++ controllers/socket-contoller.js
@@ -1,46 +1,51 @@
+// module.exports = server => {
+//     const io = require('socket.io')(server);
+//     const nsp = io.of("/talk")
+//     nsp.on('connection', function (socket) {
+//         console.log('connection');
+
+//         socket.on('chat message', function (msg) {
+//             socket.broadcast.emit('broadcast', msg);
+//         });
+
+//         socket.emit('connected_success')
+
+//     });
+
+//     io.on('connection', function (socket) {
+//         console.log('no namespace connection')
+//     })
+
+
+// }
+
 module.exports = server => {
     const io = require('socket.io')(server);
     const nsp = io.of("/talk")
+    // let rooms = []
 
-    room = ""
-    msg=""
-
-    io.sockets.on('connection', function (socket) {
-        socket.on('join', function (room) {
-            socket.join(room);
-            socket.emit('room joined')
-        });
-    });
-
-    io.sockets.on('connection', function (socket) {
-        socket.on('leave', function (room) {
-            socket.leave(room);
-            socket.emit('room left')
-        });
-    });
-
-
-    io.sockets.in(room).emit('message', msg);
-
-    nsp.on('connection', function (socket) {
-        console.log('connection');
-
-        socket.on('connection', function (socket) {
-            socket.on('chat message', function (msg) {
-                io.emit('chat message', msg);
-            });
-        });
+    io.on('connection', function (socket) {
 
         socket.emit('connected_success')
 
+        socket.on('room', (room) => {
+            socket.join(room);
+            // rooms.push(room);
+        });
+
+        socket.on('chat message', (data) => {
+            const { room, message } = data
+            console.log('got message', message);
+            socket.broadcast.to(room).emit('broadcast', message);
+        });
+
+        socket.on('disconnect', function (socket) {
+            console.log(socket)
+            socket.emit('disconnection', function (room) {
+                // socket.leave(room);
+                // var i = rooms.indexOf(room);
+                // rooms.splice(i, 1);
+            });
+        });
     });
-
-    io.on('connection', function (socket) {
-        console.log('no namespace connection')
-    })
 }
-
-
-
-
-
